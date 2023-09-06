5e9ec4165ec66182b528ce90c1481085591816d9
Andrey Knyazev <AKAKnaz@yandex.ru>
2023-09-07 00:15:41 +0300

    Stat_sever_version_test_3

--- ewm-stat/stat-server/src/main/java/ru/practicum/stats/repository/StatsRepository.java
+++ ewm-stat/stat-server/src/main/java/ru/practicum/stats/repository/StatsRepository.java
@@ -1,3 +1,46 @@
+//package ru.practicum.stats.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.stereotype.Repository;
+//import ru.practicum.stats.dto.ViewStatsDto;
+//import ru.practicum.stats.model.EndpointHit;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//@Repository
+//public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
+//
+//    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(eh.app, eh.uri, COUNT(eh.ip)) " +
+//            "FROM EndpointHit eh " +
+//            "WHERE eh.timestamp between ?1 AND ?2 " +
+//            "GROUP BY eh.app, eh.uri " +
+//            "ORDER BY COUNT(eh.ip) DESC")
+//    List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end);
+//
+//    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(eh.app, eh.uri, COUNT(DISTINCT eh.ip)) " +
+//            "FROM EndpointHit eh " +
+//            "WHERE eh.timestamp between ?1 AND ?2 " +
+//            "GROUP BY eh.app, eh.uri " +
+//            "ORDER BY COUNT(DISTINCT eh.ip) DESC")
+//    List<ViewStatsDto> getStatsUniqueIp(LocalDateTime start, LocalDateTime end);
+//
+//    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(eh.app, eh.uri,COUNT(eh.ip)) " +
+//            "FROM EndpointHit eh " +
+//            "WHERE eh.timestamp between ?1 AND ?2 AND e.uri IN(?3)" +
+//            "GROUP BY eh.app, eh.uri " +
+//            "ORDER BY COUNT(eh.ip) DESC")
+//    List<ViewStatsDto> getStatsForUris(LocalDateTime start, LocalDateTime end, List<String> uris);
+//
+//    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(eh.app, eh.uri, COUNT(DISTINCT eh.ip)) " +
+//            "FROM EndpointHit eh " +
+//            "WHERE eh.timestamp between ?1 AND ?2 AND e.uri IN(?3)" +
+//            "GROUP BY eh.app, eh.uri " +
+//            "ORDER BY COUNT(DISTINCT eh.ip) DESC")
+//    List<ViewStatsDto> getStatsUniqueIpForUris(LocalDateTime start, LocalDateTime end, List<String> uris);
+//
+//}
 package ru.practicum.stats.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,32 +55,32 @@ import java.util.List;
 @Repository
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(eh.app, eh.uri, COUNT(eh.ip)) " +
-            "FROM EndpointHit eh " +
-            "WHERE eh.timestamp between ?1 AND ?2 " +
-            "GROUP BY eh.app, eh.uri " +
-            "ORDER BY COUNT(eh.ip) DESC")
+    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(e.app, e.uri, COUNT(e.ip)) " +
+            "FROM EndpointHit as e " +
+            "WHERE e.timestamp between ?1 AND ?2 " +
+            "GROUP BY e.app, e.uri " +
+            "ORDER BY COUNT(e.ip) DESC")
     List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(eh.app, eh.uri, COUNT(DISTINCT eh.ip)) " +
-            "FROM EndpointHit eh " +
-            "WHERE eh.timestamp between ?1 AND ?2 " +
-            "GROUP BY eh.app, eh.uri " +
-            "ORDER BY COUNT(DISTINCT eh.ip) DESC")
+    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
+            "FROM EndpointHit as e " +
+            "WHERE e.timestamp between ?1 AND ?2 " +
+            "GROUP BY e.app, e.uri " +
+            "ORDER BY COUNT(DISTINCT e.ip) DESC")
     List<ViewStatsDto> getStatsUniqueIp(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(eh.app, eh.uri,COUNT(eh.ip)) " +
-            "FROM EndpointHit eh " +
-            "WHERE eh.timestamp between ?1 AND ?2 AND e.uri IN(?3)" +
-            "GROUP BY eh.app, eh.uri " +
-            "ORDER BY COUNT(eh.ip) DESC")
+    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(e.app, e.uri,COUNT(e.ip)) " +
+            "FROM EndpointHit as e " +
+            "WHERE e.timestamp between ?1 AND ?2 AND e.uri IN(?3)" +
+            "GROUP BY e.app, e.uri " +
+            "ORDER BY COUNT(e.ip) DESC")
     List<ViewStatsDto> getStatsForUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(eh.app, eh.uri, COUNT(DISTINCT eh.ip)) " +
-            "FROM EndpointHit eh " +
-            "WHERE eh.timestamp between ?1 AND ?2 AND e.uri IN(?3)" +
-            "GROUP BY eh.app, eh.uri " +
-            "ORDER BY COUNT(DISTINCT eh.ip) DESC")
+    @Query("SELECT new ru.practicum.stats.dto.ViewStatsDto(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
+            "FROM EndpointHit as e " +
+            "WHERE e.timestamp between ?1 AND ?2 AND e.uri IN(?3)" +
+            "GROUP BY e.app, e.uri " +
+            "ORDER BY COUNT(DISTINCT e.ip) DESC")
     List<ViewStatsDto> getStatsUniqueIpForUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
 }
